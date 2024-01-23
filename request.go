89b22ddd7a0d9357389ad7829c037539d86afe9c
liujinyin <liujinyin@xunyou.com>
2024-01-23 16:26:43 +0800

    初始化代码

---
+++ request.go
@@ -0,0 +1,19 @@
+package alloter
+
+import (
+	"context"
+	"net/url"
+)
+
+type IRequest interface {
+	Context() context.Context
+	WithContext(context.Context)
+	GetName() string
+	//GetService() string
+	GetURL() *url.URL
+	GetMethod() string
+	Params() map[string]string
+	GetHeader() map[string]string
+	Body() []byte
+	GetRemoteAddr() string
+}
